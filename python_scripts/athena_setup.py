660a5613f81febd143166e6076bd5ba2446814bd
amankumar-dev <rjvkha@gmail.com>
2026-09-09 19:26:12 +0530

    athena query

--- python_scripts/athena_setup.py
+++ python_scripts/athena_setup.py
@@ -39,3 +39,81 @@ def create_database():
     print(f"Creating database '{DATABASE_NAME}' (if not exists)...")
     run_ddl(f"CREATE DATABASE IF NOT EXISTS {DATABASE_NAME}")
     
+def create_tables():
+    tables={
+        "dim_customers":'''
+            CREATE EXTERNAL TABLE IF NOT EXISTS dim_customers(
+                cust_id STRING,
+                cust_unq_id STRING,
+                cust_name STRING,
+                cust_city STRING,
+                cust_state STRING,
+                cust_zipcode DOUBLE   
+            )
+            STORED AS PARQUET
+            LOCATION 's3://{bucket}/gold/dim_customers/'
+        ''',
+        
+        "dim_products":'''
+            CREATE EXTERNAL TABLE IF NOT EXISTS dim_products(
+                prod_id STRING,
+                prod_cat_name STRING,
+                prod_cat_name_eng STRING,
+                prod_weight_g DOUBLE,
+                prod_length_cm DOUBLE,
+                prod_height_cm DOUBLE,
+                prod_width_cm DOUBLE
+            )
+            STORED AS PARQUET
+            LOCATION 's3://{bucket}/gold/dim_products/'
+        ''',
+        
+        "dim_sellers":'''
+            CREATE EXTERNAL TABLE IF NOT EXISTS dim_sellers(
+                seller_id STRING,
+                seller_city STRING,
+                seller_state STRING,
+                seller_zipcode BIGINT
+            )
+            STORED AS PARQUET
+            LOCATION 's3://{bucket}/gold/dim_sellers/'
+        ''',
+        
+        "fact_order_items":'''
+            CREATE EXTERNAL TABLE IF NOT EXISTS fact_order_items(
+                order_id STRING,
+                order_item_id BIGINT,
+                prod_id STRING,
+                seller_id STRING,
+                shipp_limit_date STRING,
+                price DOUBLE,
+                freight_val DOUBLE,
+                `timestamp` TIMESTAMP,
+                source STRING,
+                batch_id STRING,
+                cust_id STRING,
+                ord_status STRING,
+                purchase_timestamp TIMESTAMP,
+                delivered_customer_date TIMESTAMP,
+                estimated_delivery_date TIMESTAMP,
+                order_year_month STRING,
+                cust_state STRING,
+                order_payment_value_total DOUBLE,
+                item_revenue DOUBLE
+            )
+            STORED AS PARQUET
+            LOCATION 's3://{bucket}/gold/fact_order_items/'
+        '''
+    }
+    
+    for table_name,ddl_template in tables.items():
+        print(f"Creating table '{table_name}'...")
+        ddl=ddl_template.format(bucket=BUCKET_NAME)
+        run_ddl(ddl,database=DATABASE_NAME)
+
+        
+create_database()
+create_tables()
+print(f"\n✅ Athena setup complete. Database: {DATABASE_NAME}")
+print("Query results will land in:", ATHENA_OUTPUT)
+
